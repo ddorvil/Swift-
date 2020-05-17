fb224193b62500dfaeecbc5256a962d9253581ca
ddorvil <56710675+ddorvil@users.noreply.github.com>
2020-05-17 00:51:02 -0400

    Add files via upload

---
+++ Tamagotchi/Tamagotchi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tamagotchi
+//
+//  Created by Dominique Dorvil on 9/7/19.
+//  Copyright © 2019 Dominique Dorvil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+class
